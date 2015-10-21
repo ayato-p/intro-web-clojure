561c6ba9eb5677c793938765f3511465321708aa
ayato_p <ayato.skycrawlers@gmail.com>
2015-10-21 13:57:53 +0900

    Ring を使った Hello, world

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -1,6 +1,13 @@
-(ns todo-clj.core)
+(ns todo-clj.core
+  (:require [ring.adapter.jetty :as server]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce server (atom nil))
+
+(defn handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn start-server []
+  (if-not @server
+    (reset! server (server/run-jetty handler {:port 3000 :join? false}))))
