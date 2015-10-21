c40de77abd80a39011939fcca1193ad0a86f01aa
ayato_p <ayato.skycrawlers@gmail.com>
2015-10-21 22:42:18 +0900

    ルーティング機能を実装してみる

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -3,10 +3,57 @@
 
 (defonce server (atom nil))
 
-(defn handler [req]
+(defn ok [body]
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world"})
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</1>"})
+
+(defn home-view [req]
+  "<h1>ホーム画面</h1>
+   <a href=\"/todo\">TODO 一覧</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(def todo-list
+  [{:title "朝ごはんを作る"}
+   {:title "燃えるゴミを出す"}
+   {:title "卵を買って帰る"}
+   {:title "お風呂を洗う"}])
+
+(defn todo-index-view [req]
+  `("<h1>TODO 一覧</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} todo-list]
+        (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+(defn match-route [uri]
+  (get routes uri))
+
+(defn handler [req]
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
 
 (defn start-server []
   (when-not @server
