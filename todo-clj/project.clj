67f7d6b99bee4b1ffbfb469d4fdf7c0f25840e8c
ayato_p <ayato.skycrawlers@gmail.com>
2015-11-13 08:10:25 +0900

    jdbc を依存関係へと追加

--- todo-clj/project.clj
+++ todo-clj/project.clj
@@ -7,7 +7,9 @@
                  [ring "1.4.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [environ "1.0.1"]]
+                 [environ "1.0.1"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.postgresql/postgresql "9.4-1205-jdbc42"]]
   :plugins [[lein-environ "1.0.1"]]
   :profiles
   {:dev {:dependencies [[prone "0.8.2"]]
